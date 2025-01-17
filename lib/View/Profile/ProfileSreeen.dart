f09da0a8d2eb9bcd04c60f8a34ad6432832200a4
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-01-17 21:47:38 +0500

    1.Making comic app

--- lib/View/Profile/ProfileSreeen.dart
+++ lib/View/Profile/ProfileSreeen.dart
@@ -10,7 +10,6 @@ class ProfileScreen extends StatefulWidget {
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
 }
-
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
