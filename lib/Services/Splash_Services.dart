c2a41a82d1e44b406a5a6c60ee03847c5fff8f6f
Huzaifa atta <huzaifaatta8152@gmail.com>
2025-01-21 15:29:06 +0500

    1.Making comic app

--- lib/Services/Splash_Services.dart
+++ lib/Services/Splash_Services.dart
@@ -1,11 +1,9 @@
 import 'package:comic_reading_app/View/Authentication/LoginScreen/LoginScreen.dart';
-import 'package:comic_reading_app/View/Authentication/SignUpScreen/SignUpScreen.dart';
 import 'package:comic_reading_app/View/DashBoardScreen/DashBoardScreen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class Services {
